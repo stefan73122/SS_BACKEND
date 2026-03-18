28bf9ab7725fc5f6142068bca603361ecf34aad2
Ivo estephan concepción <shelterdeneulin@gmail.com>
2026-03-18 10:20:39 -0400

    fix: Quitar requirePermission de rutas de units para permitir acceso a usuarios autenticados

--- src/routes/unit.routes.js
+++ src/routes/unit.routes.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const unitController = require('../controllers/unit.controller');
 const authMiddleware = require('../middlewares/authMiddleware');
-const { requirePermission } = require('../middlewares/permissionMiddleware');
 
 const router = express.Router();
 
-router.get('/', authMiddleware, requirePermission('units.view'), unitController.getAllUnits);
-router.get('/:id', authMiddleware, requirePermission('units.view'), unitController.getUnitById);
+router.get('/', authMiddleware, unitController.getAllUnits);
+router.get('/:id', authMiddleware, unitController.getUnitById);
 
 module.exports = router;
